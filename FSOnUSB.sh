e8b7a08a261ed579f139f53840cee3bb05a35911
Spox03 <pomme033@gmail.com>
2017-10-29 23:55:46 +0100

    Pass the file system on USB key

---
+++ FSOnUSB.sh
@@ -0,0 +1,5 @@
+# Make a USB file system
+# For now i use the default tool "SD copier" who work to copy a running system (TODO : research)
+ls -l /dev/disk/by-partuuid/
+# add to cmdline.txt 
+# root==partuuid==[uuid]
